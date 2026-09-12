46dba8ea38f5c7c7482b54640aaf7983d7a8e5ae
Akhilesh Gupta <akhil.smile26@gmail.com>
2026-09-12 08:31:33 +0530

    Enhance basics.java with HashMap examples
    Updated HashMap usage with new key-value pairs and added traversal examples.

--- ArraysAndStrings/HashMap/basics.java
+++ ArraysAndStrings/HashMap/basics.java
@@ -2,12 +2,45 @@ package ArraysAndStrings.HashMap;
 import java.util.*;
 public class basics {
     public static void main(String[] args) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        //(1)put
-        hm.put(1,11);
-        hm.put(2,22);
-        //(2)If already present update
-        hm.put(2,20);
+         HashMap<Integer,Integer> hm=new HashMap<>();
+        hm.put(10,20);
+        hm.put(50,100);
+        hm.put(10,20);
+        System.out.println(hm);
+        // key travel
+        for(int key:hm.keySet()){
+            System.out.println(key);
+            System.out.println(hm.get(key));
+        }
+        //value trqversal
+        for(int val:hm.values()){
+            System.out.println(val);
+        }
+        // travel at key value simultaneously
+        for(var v:hm.entrySet()){
+            int key=v.getKey();
+            int val=v.getValue();
+            System.out.println(key+"->"+val);
+        }
+        System.out.println(hm.get(50));
+        System.out.println(hm.containsValue(100));
+        ArrayList<Map.Entry<Integer,Integer>> list=new ArrayList<>(hm.entrySet());
+        System.out.println(list);
+
+
+        //OUTPUT
+ {50=100, 10=20}
+50
+100
+10
+20
+100
+20
+50->100
+10->20
+100
+true
+[50=100, 10=20]
         
 
     }
